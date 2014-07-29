dbbc5f6fac708089743fc58d1083abfa44a900c0
U-bgpark-PC\bgpark <bgpark@bgpark-PC.(none)>
2014-07-29 20:55:34 +0900

    DELETE : obj

--- example/android/obj/convert-dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# AUTO-GENERATED FILE, DO NOT EDIT!
-if [ -f $1.org ]; then
-  sed -e 's!^D:/cygwin/lib!/usr/lib!ig;s! D:/cygwin/lib! /usr/lib!ig;s!^D:/cygwin/bin!/usr/bin!ig;s! D:/cygwin/bin! /usr/bin!ig;s!^D:/cygwin/!/!ig;s! D:/cygwin/! /!ig;s!^Z:!/cygdrive/z!ig;s! Z:! /cygdrive/z!ig;s!^D:!/cygdrive/d!ig;s! D:! /cygdrive/d!ig;s!^C:!/cygdrive/c!ig;s! C:! /cygdrive/c!ig;' $1.org > $1 && rm -f $1.org
-fi
